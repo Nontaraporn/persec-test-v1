71202c91fbd08cc13649c93a8cd1d643a581bffa
Dev`Tar <tatami.non@gmail.com>
2026-03-02 21:46:23 +0700

    Push 02 Mar 26 [09:46]
    #Fix
     - comment code that doesn't user

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import './App.css'
 import Login from "./pages/Login";
 import Register from "./pages/Register";
